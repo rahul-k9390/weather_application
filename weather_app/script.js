ea08462b6d592dc41ccec2811dd23f4adf10e5d3
Rahul kumar <kumarrahul9390@gmail.com>
2026-03-08 20:47:29 +0530

    first commit

---
+++ weather_app/script.js
@@ -0,0 +1,48 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img= document.querySelector('.weather-img')
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+async function checkWeather(city){
+    const api_key = "29ee686335d1e3eaaf6a2e113e115365";
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const response = await fetch(url);
+    const weather_data = await response.json();
+
+    temperature.innerHTML= `${Math.round(weather_data.main.temp - 273.15)}°C`
+    description.innerHTML =`${weather_data.weather[0].description}`
+    humidity.innerHTML=`${weather_data.main.humidity}%`
+    wind_speed.innerHTML=`${weather_data.wind.speed}KM/H`
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds' :
+            weather_img.src="./Assests/cloud.png";
+            break;
+        case 'Clear':
+        weather_img.src="./Assests/clear.png";
+        break;
+        case 'Rain' :
+            weather_img.src="./Assests/rain.png";
+            break;
+        case 'Mist' :
+        weather_img.src="./Assests/mist.png";
+            break;
+        case 'Snow' :
+            weather_img.src="./Assests/snow.png";
+            break;
+            
+    }
+
+    
+}
+
+
+searchBtn.addEventListener('click',()=>{
+    checkWeather(inputBox.value)
+})
+
+
